95bfb0114a99afe72e6e0b7e60e356a6c619d36b
Astrid Sawatzky <astridemma@users.sourceforge.net>
2018-09-04 18:44:52 +0200

    implemented new Datfunk and its tests

--- RehaCommon/src/CommonTools/DatFunk.java
+++ RehaCommon/src/CommonTools/DatFunk.java
@@ -1,329 +1,134 @@
 package CommonTools;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-//import java.text.ParseException;
-//import javax.swing.JFormattedTextField;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
+import java.time.DayOfWeek;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.Year;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.format.TextStyle;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalAdjusters;
+import java.time.temporal.WeekFields;
 import java.util.Locale;
-
-
-
-
 
 public class DatFunk {
 
+    private static final DateTimeFormatter inFormatter = DateTimeFormatter.ofPattern("d.M.yyyy");
+    private static final DateTimeFormatter outFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static DateTimeFormatter reverseHyphonFormatter = DateTimeFormatter.ofPattern("yyyy-M-d");
 
-//private String sDatumJava = "";
-//private String sDatumDeutsch = "";
+    public static String sDatInDeutsch(String sJavaDat) {
+        if (sJavaDat == null) {
+            return "  .  .    ";
+        }
 
-	public static String sDatInDeutsch(String sJavaDat){
-		if(sJavaDat==null){
-			return "  .  .    ";
-		}
-		//if(sJavaDat.length() != 10){
-		//	return "  .  .    ";
-		//}
-		String[] splittArray = sJavaDat.split("-");
-		return splittArray[2]+"."+splittArray[1]+"."+splittArray[0];
-	}
-	
-	public static String sDatInSQL(String sDeutschDat){
-		//String sDatumSQL= new String();
-		String[] splittArray = sDeutschDat.split("\\.");
-		//sDatumSQL = splittArray[2]+"-"+splittArray[1]+"-"+splittArray[0]; 
-		//System.out.println(sDatumSQL);
-		return  splittArray[2]+"-"+splittArray[1]+"-"+splittArray[0];
-	}
+        String[] splittArray = sJavaDat.split("-");
+        return splittArray[2] + "." + splittArray[1] + "." + splittArray[0];
+    }
 
-	public static String sHeute(){
-		DateFormat df = DateFormat.getDateTimeInstance( DateFormat.MEDIUM,
-				DateFormat.MEDIUM,  
-                Locale.GERMANY );
-		String s = df.format(new java.util.Date());
-		s = s.substring(0,s.indexOf( ' ' )); 
-	    return s;
+    public static String sDatInSQL(String sDeutschDat) {
+       return LocalDate.parse(sDeutschDat, inFormatter).format(reverseHyphonFormatter);
+       
+    }
 
-	}
-/*************************************************************************************/	
-	public static String sDatPlusTage(String datum,int Tage){
-		long anz_milli_1 = 0;
-		long anz_milli_2 = 0;
-		long anz_milli_3 = 0;
-		//boolean deutsch = false;
-		if(Tage==0){
-			return datum;
-		}
-		String[] datsplit = datum.split("\\."); 
-		Date dDatum;
-		String s;
-		Calendar cal1 = new GregorianCalendar();
-	    cal1.set( Integer.parseInt(datsplit[2]), Integer.parseInt(datsplit[1])-1, Integer.parseInt(datsplit[0]) );
- 
-		anz_milli_1 = cal1.getTime().getTime();
-		anz_milli_2 = Math.round( Tage* (24. * 60.*60.*1000.) );
-		anz_milli_3 = anz_milli_1+anz_milli_2;		
-		
+    public static String sHeute() {
+        return LocalDate.now().format(outFormatter);
 
-		DateFormat df = DateFormat.getDateTimeInstance( DateFormat.MEDIUM,
-					DateFormat.MEDIUM,  
-					Locale.GERMANY);
+    }
 
-		if (anz_milli_2==0){
-			//dDatum = new Date();
-			dDatum = new Date(DatFunk.DatumsWert(datum));
-		}else{
-			dDatum = new Date(anz_milli_3);
-		}
-		s = df.format(dDatum);
-		s = s.substring(0,s.indexOf( ' ' )); 
-		return s;
-	}
-/*************************************************************************************/	
-	
-	public static String WochenTag(String sdatum) {
-		String[] asDatTeil = sdatum.split("\\.");
-		SimpleDateFormat formatter = new SimpleDateFormat("dd.mm.yyyy");
-		
-		formatter.applyPattern( "EEEE', 'dd.mm yyyy hh:mm" );
-		
-		String sErgebnis = formatter.format(new Date(Integer.parseInt(asDatTeil[2]),
-										Integer.parseInt(asDatTeil[1])-1,
-										Integer.parseInt(asDatTeil[0])-1 ));
-		String[] asErgebnis = sErgebnis.split(",");
-	return asErgebnis[0];
-	}
-	
-	
-	public static int TagDerWoche(String sdatum) {
-		int tag=0;
-		String[] datsplit = sdatum.split("\\.");
-		Calendar cal1 = new GregorianCalendar();
-		cal1.set( Integer.parseInt(datsplit[2]), Integer.parseInt(datsplit[1])-1, Integer.parseInt(datsplit[0]),0,0,0 ); 
-		tag = cal1.get(Calendar.DAY_OF_WEEK); 
-		if(tag == 1){
-			tag = 7; 
-		}else{
-			tag = tag - 1;
-		}
-		return tag;
-	}
+    public static String sDatPlusTage(String datum, int Tage) {
+        LocalDate datum1 = LocalDate.parse(datum, inFormatter);
+        LocalDate later = datum1.plusDays(Tage);
 
-	public static long TageDifferenz(String saltdatum,String sneudatum){
-		long mul1 = 0;
-		long mul2 = 0;
-		long mul3 = 0;
+        return later.format(outFormatter);
+    }
 
-		String[] datsplit1 = saltdatum.split("\\.");
-		String[] datsplit2 = sneudatum.split("\\.");
-		Calendar cal1 = new GregorianCalendar();
-		Calendar cal2 = new GregorianCalendar();
-		cal1.set( Integer.parseInt(datsplit1[2]), Integer.parseInt(datsplit1[1])-1, Integer.parseInt(datsplit1[0]),0,0,0 ); 
-		cal2.set( Integer.parseInt(datsplit2[2]), Integer.parseInt(datsplit2[1])-1, Integer.parseInt(datsplit2[0]),0,0,0 );
+    public static String WochenTag(String datum) {
+        LocalDate datum1 = LocalDate.parse(datum, inFormatter);
+        return datum1.getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.GERMAN);
 
-		mul1 = cal1.getTimeInMillis();
-		mul2 = cal2.getTimeInMillis();
-		
-		mul3 = mul2-mul1;
-		if(mul3==0){
-			return 0;
-		}
-		return Math.round( mul3 / (24. * 60.*60.*1000.) );
-		//return  mul3/(24*60*60*1000L);
-	}
+    }
 
-	public static String WocheErster(String sdatum){
-		long mul = 0;
-		long mul2 = 0;
-		int tag, monat, jahr;
-		String[] datsplit = sdatum.split("\\.");
-		Calendar cal1 = new GregorianCalendar();
-		cal1.set( Integer.parseInt(datsplit[2]), Integer.parseInt(datsplit[1])-1, Integer.parseInt(datsplit[0]) ); 
-		tag = cal1.get(Calendar.DAY_OF_WEEK); 
-		if(tag == 1){
-			tag = 7; 
-		}else{
-			tag = tag - 1;
-		}
-		mul = (tag-1)*24*60*60*1000L;
-		mul2 = cal1.getTimeInMillis();
-		cal1.setTimeInMillis(mul2-mul);
-		
-		tag   = cal1.get(Calendar.DATE);
-		monat = cal1.get(Calendar.MONTH)+1;
-		jahr  = cal1.get(Calendar.YEAR);
-		String a = (Integer.valueOf(tag)).toString();
-		a= (a.length()<2 ? "0"+a : a);
-		String b = (Integer.valueOf(monat)).toString();
-		b= (b.length()<2 ? "0"+b : b);
-		return a+"."+b+"."+Integer.toString(jahr);
-	}
-	public static String WocheLetzter(String sdatum){
-		long mul = 0;
-		long mul2 = 0;
-		int tag, monat, jahr;
-		String serster = WocheErster(sdatum);
-		String[] datsplit = serster.split("\\.");
-		Calendar cal1 = new GregorianCalendar();		
-		cal1.set( Integer.parseInt(datsplit[2]), Integer.parseInt(datsplit[1])-1, Integer.parseInt(datsplit[0]) );		
-		mul = 6*24*60*60*1000L;
-		mul2 = cal1.getTimeInMillis();
-		cal1.setTimeInMillis(mul2+mul);
-		
-		tag   = cal1.get(Calendar.DATE);
-		monat = cal1.get(Calendar.MONTH)+1;
-		jahr  = cal1.get(Calendar.YEAR);
-		String a = Integer.toString(tag);
-		a= (a.length()<2 ? "0"+a : a);
-		String b = Integer.toString(monat);
-		b= (b.length()<2 ? "0"+b : b);
-		return a+"."+b+"."+jahr;
-	}
-	public String WocheNaechste(String sdatum){
-		return "";
-	}
-	public static int KalenderWoche(String sdatum){
-		String[] datsplit = sdatum.split("\\.");
-		Calendar gcal =  Calendar.getInstance();
-	    gcal.set( Integer.parseInt(datsplit[2]), Integer.parseInt(datsplit[1])-1, Integer.parseInt(datsplit[0]) ); 
-	    return gcal.get(Calendar.WEEK_OF_YEAR);
-	}
-	public static long DatumsWert(String sdatum){
-		Calendar gcal = new GregorianCalendar();
-		if(sdatum.contains(".")){
-			String[] datsplit = sdatum.split("\\.");
-			gcal.set( Integer.parseInt(datsplit[2]), 
-					Integer.parseInt( (datsplit[1].startsWith("0") ? datsplit[1].substring(1,2) : datsplit[1]) )-1,
-					Integer.parseInt(datsplit[0].startsWith("0") ? datsplit[0].substring(1,2) : datsplit[0]),0,0,0);
-		}else{
-			String[] datsplit = sdatum.split("-");
-			gcal.set( Integer.parseInt(datsplit[0]),
-					Integer.parseInt( (datsplit[1].startsWith("0") ? datsplit[1].substring(1,2) : datsplit[1]) ) -1 ,
-					Integer.parseInt(datsplit[2].startsWith("0") ? datsplit[2].substring(1,2) : datsplit[2]),0,0,0);
-		}
-	    return gcal.getTime().getTime();
-	}
-	
-	public static String WertInDatum(long ldatum){
-		String s;
-		Date dDatum;
-		DateFormat df = DateFormat.getDateTimeInstance( DateFormat.MEDIUM,
-				DateFormat.MEDIUM,  
-				Locale.GERMANY);
+    public static int TagDerWoche(String sdatum) {
 
-		if (ldatum==0){
-			dDatum = new Date();
-		}else{
-			dDatum = new Date(ldatum);
-		}
-		s =df.format(dDatum);
-		s = s.substring(0,s.indexOf( ' ' )); 
-	    return s;
-	}
-	public static String datumOk(String datum){
-		if(datum==null){
-			return null;
-		}
-		if(datum.trim().equals("") || datum.trim().equals(".  .") || datum.trim().equals("-  -")){
-			return null;
-		}
-		return DatFunk.sDatInDeutsch(datum);
-	}	
-	public static boolean GeradeWoche(String sdatum){
-		return ((KalenderWoche(sdatum) % 2)!= 0 ? false : true);
-	}
-	
-	public static boolean Unter18(String bezugdat,String geburtstag){
-		String[] datsplit = bezugdat.split("\\.");
-		int jahr = Integer.valueOf(datsplit[2]) - 18;
-		String testdatum = datsplit[0]+"."+datsplit[1]+"."+Integer.toString(jahr);
-		if(DatumsWert(testdatum) < DatumsWert(geburtstag)){
-			return true;
-		}else{
-			return false;			
-		}
-	}
-	
-	public static boolean Schaltjahr(int jahr) {
-		if( jahr == 0 ) {
-		    //System.out.println("Es gibt kein Jahr 0!");
-			return false;
-		} else {
-			if( jahr % 4 == 0 ) {
-				if( jahr % 100 == 0 ) {
-					if( jahr % 400 == 0 ) {
-						//System.out.println("Schaltjahr!");
-						return true;
-					} else {
-						//System.out.println("Kein Schaltjahr!");
-						return false;
-					}
-				} else {
-			    //System.out.println("Schaltjahr!");
-				return true;
-				}
-			} else {
-				//System.out.println("Kein Schaltjahr!");
-				return false;
-			}
-		}
-	}
+        LocalDate datum = LocalDate.parse(sdatum, inFormatter);
+        return datum.getDayOfWeek().getValue();
 
-	public static int JahreDifferenz(String sAktuellesJahr,String sInputJahr) {
-		int aktuell = Integer.valueOf(sAktuellesJahr.substring(6));
-		int input = Integer.valueOf(sInputJahr.substring(6));
-		return (aktuell-input);
-	}
+    }
 
-	/********************Klassen-Klammer*************/
+    public static long TageDifferenz(String sdatum1, String sdatum2) {
+        LocalDate datum1 = LocalDate.parse(sdatum1, inFormatter);
+        LocalDate datum2 = LocalDate.parse(sdatum2, inFormatter);
+
+        return ChronoUnit.DAYS.between(datum1, datum2);
+    }
+
+    public static String WocheErster(String sdatum) {
+        LocalDate ld = LocalDate.parse(sdatum, inFormatter);
+        return ld.with(TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY)).format(outFormatter);
+    }
+
+    public static String WocheLetzter(String sdatum) {
+        LocalDate ld = LocalDate.parse(sdatum, inFormatter);
+        return ld.with(TemporalAdjusters.nextOrSame(DayOfWeek.SUNDAY)).format(outFormatter);
+
+    }
+   
+    public static int KalenderWoche(String sdatum) {
+        LocalDate ld = LocalDate.parse(sdatum, inFormatter);
+        return ld.get(WeekFields.of(Locale.GERMANY).weekOfYear());
+    }
+    
+    /**
+     * converts a given date into its long value (milliseconds since 01011970)
+     * 
+     * if the date is separated with hyphons it needs to be in format yyyy-M-d
+     * @param sdatum 
+     * @return
+     */
+    public static long DatumsWert(String sdatum) {
+        LocalDate ld;
+        if (sdatum.contains(".")) {
+            ld = LocalDate.parse(sdatum, inFormatter);
+
+        } else {
+            ld = LocalDate.parse(sdatum, reverseHyphonFormatter);
+        }
+
+        ZoneId zoneId = ZoneId.systemDefault();
+        return ld.atStartOfDay(zoneId).toInstant().toEpochMilli();
+    }
+    
+    public static String WertInDatum(long ldatum) {
+        
+        Instant inst;
+        if (ldatum == 0) {
+            inst = Instant.now();
+        } else {
+            inst = Instant.ofEpochMilli(ldatum);
+        }
+        return inst.atZone(ZoneId.systemDefault()).toLocalDate().format(outFormatter);
+    }
+
+    public static boolean GeradeWoche(String sdatum) {
+        return (KalenderWoche(sdatum) % 2) == 0 ;
+    }
+    public static boolean Unter18(String bezugdat, String geburtstag) {
+        LocalDate geb = LocalDate.parse(geburtstag, outFormatter);
+        LocalDate bezug = LocalDate.parse(bezugdat, outFormatter);
+        return ChronoUnit.YEARS.between(geb, bezug) < 18;
+    }
+
+    public static boolean Schaltjahr(int jahr) {
+        return Year.isLeap(jahr);
+
+    }
+
+    public static int JahreDifferenz(String sAktuellesJahr, String sInputJahr) {
+        LocalDate aktuell = LocalDate.parse(sAktuellesJahr, outFormatter);
+        LocalDate vergleich = LocalDate.parse(sInputJahr, outFormatter);
+
+        return (int) ChronoUnit.YEARS.between(vergleich, aktuell);
+    }
+
 }
-	
-
-/*
- *
- * /-------------------------------------------
-//Daten aufnehmen
-SimpleDateFormat formatter = new SimpleDateFormat("dd.mm.yyyy");
-Date spaeter = formatter.parse("17.10.2004");
-Date frueher = formatter.parse("13.10.2004");
-
-//Millisekunden seit 1970 ;-)
-long timeSpaeter = spaeter.getTime();
-long timeFrueher = frueher.getTime();
-
-//Millisekunden --> Tage
-long diff = (timeSpaeter - timeFrueher) / 24 / 60 / 60 / 1000;
-
-System.out.println("Vergangen: " + diff + " Tage !");
-//-----------------------------------------
-
-*
-    // heutiges Datum wird erzeugt
-    Date heute = new Date();
-
-    // Anzahl der Sekunden nach 1.1.1970 ermitteln
-    long anz_milli_sekunden_nach_1970 = heute.getTime();
- 
-    // heutiges Datum wird erzeugt
-    long milli_sekunden_gestern = anz_milli_sekunden_nach_1970
-                            - 24L*60L*60L*1000L;
-
-    // gestriges Datum wird erzeugt
-    Date gestern = new Date(milli_sekunden_gestern);
-  // heutiges Datum wird erzeugt
-	    Date heute = new Date();
-
-	    // Anzahl der Sekunden nach 1.1.1970 ermitteln
-	    long anz_milli_sekunden_nach_1970 = heute.getTime();
-	 
-	    // heutiges Datum wird erzeugt
-	    long milli_sekunden_gestern = anz_milli_sekunden_nach_1970
-	                            - 24L*60L*60L*1000L;
-
-	    // gestriges Datum wird erzeugt
-	    Date gestern = new Date(milli_sekunden_gestern);
-
-
-*/
-
---
+++ RehaCommon/tests/CommonTools/DatFunkTest.java
@@ -0,0 +1,143 @@
+package CommonTools;
+
+import static org.junit.Assert.assertEquals;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import org.junit.Test;
+
+public class DatFunkTest extends DatFunk {
+
+    @Test
+    public void testSDatInDeutsch() {
+       assertEquals("  .  .    ", DatFunk.sDatInDeutsch(null));
+       assertEquals("21.03.2012", DatFunk.sDatInDeutsch("2012-03-21"));
+       assertEquals("1.1.2012", DatFunk.sDatInDeutsch("2012-1-1"));
+       
+    }
+
+    @Test
+    public void testSDatInSQL() {
+        assertEquals("2022-5-1", DatFunk.sDatInSQL("1.5.2022"));
+    }
+
+    @Test
+    public void testSHeute() {
+        LocalDate date = LocalDate.now();
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        
+       assertEquals(date.format(dtf), DatFunk.sHeute());
+    }
+
+    @Test
+    public void testSDatPlusTage() {
+       assertEquals("02.03.2011", DatFunk.sDatPlusTage("28.02.2011", 2));
+       assertEquals("01.03.2012", DatFunk.sDatPlusTage("28.02.2012", 2));
+       assertEquals("01.03.2000",DatFunk.sDatPlusTage("28.02.2000", 2)); 
+    }
+
+    @Test
+    public void testWochenTag() {
+       assertEquals("Montag",DatFunk.WochenTag("28.02.2011"));
+       assertEquals("Sonntag",DatFunk.WochenTag("06.02.2011"));
+    }
+
+    @Test
+    public void testTagDerWoche() {
+        assertEquals(1,DatFunk.TagDerWoche("28.02.2011"));
+        assertEquals(7,DatFunk.TagDerWoche("6.02.2011"));
+  
+    }
+
+    @Test
+    public void testTageDifferenz() {
+        assertEquals(365,DatFunk.TageDifferenz("28.02.2011","28.02.2012"));
+        assertEquals(366,DatFunk.TageDifferenz("28.02.2012","28.02.2013"));
+        assertEquals(365,DatFunk.TageDifferenz("28.02.2013","28.02.2014"));
+        
+        assertEquals(-8,DatFunk.TageDifferenz("28.02.2013","20.02.2013"));
+        
+    }
+
+    @Test
+    public void testWocheErster() {
+        assertEquals("28.02.2011", DatFunk.WocheErster("28.02.2011"));
+        assertEquals("27.02.2012", DatFunk.WocheErster("28.02.2012"));
+        
+        assertEquals("25.02.2013", DatFunk.WocheErster("28.02.2013"));
+    }
+
+    @Test
+    public void testWocheLetzter() {
+        assertEquals("06.03.2011", DatFunk.WocheLetzter("28.02.2011"));
+        assertEquals("04.03.2012", DatFunk.WocheLetzter("28.02.2012"));
+        
+        assertEquals("03.03.2013", DatFunk.WocheLetzter("28.02.2013"));
+ 
+    }
+
+    
+    @Test
+    public void testKalenderWoche() {
+        assertEquals(9, DatFunk.KalenderWoche("28.02.2011"));
+        assertEquals(9, DatFunk.KalenderWoche("28.02.2012"));
+        
+        assertEquals(9, DatFunk.KalenderWoche("28.02.2013"));
+        assertEquals(13, DatFunk.KalenderWoche("28.03.2013"));
+        assertEquals(17, DatFunk.KalenderWoche("28.04.2013"));
+        
+        assertEquals(22, DatFunk.KalenderWoche("28.05.2013"));
+ 
+    }
+
+    @Test
+    public void testDatumsWert() {
+        assertEquals("28.05.2013", DatFunk.WertInDatum(DatFunk.DatumsWert("2013-05-28")));
+
+        assertEquals("28.05.2013", DatFunk.WertInDatum(DatFunk.DatumsWert("28.05.2013")));
+    
+    }
+
+    @Test
+    public void testWertInDatum() {
+        assertEquals("01.01.1970", DatFunk.WertInDatum(1L));
+    }
+
+    @Test
+    public void testGeradeWoche() {
+        assertEquals(false, DatFunk.GeradeWoche("28.02.2013"));
+        assertEquals(false, DatFunk.GeradeWoche("28.03.2013"));
+        assertEquals(false, DatFunk.GeradeWoche("28.04.2013"));
+        
+        assertEquals(true, DatFunk.GeradeWoche("28.05.2013"));
+    }
+
+    @Test
+    public void testUnter18() {
+        assertEquals(false, DatFunk.Unter18("20.02.2018", "19.02.2000"));
+         assertEquals(true, DatFunk.Unter18("20.02.2018", "21.02.2000"));
+         assertEquals(false, DatFunk.Unter18("20.02.2018", "20.02.2000")); 
+    }
+
+    @Test
+    public void testSchaltjahr() {
+        assertEquals(true, DatFunk.Schaltjahr(2000)); 
+        assertEquals(false, DatFunk.Schaltjahr(2001));
+        assertEquals(true, DatFunk.Schaltjahr(2004));
+        assertEquals(false, DatFunk.Schaltjahr(2005));
+        assertEquals(false, DatFunk.Schaltjahr(2100));
+    }
+
+    @Test
+    public void testJahreDifferenz() {
+        assertEquals(18, DatFunk.JahreDifferenz("20.02.2018", "19.02.2000")); 
+        assertEquals(18, DatFunk.JahreDifferenz("20.02.2018", "20.02.2000")); 
+        assertEquals(17, DatFunk.JahreDifferenz("20.02.2018", "21.02.2000"));
+        assertEquals(18, DatFunk.JahreDifferenz("20.02.2018", "21.02.1999"));
+        assertEquals(0,  DatFunk.JahreDifferenz("01.01.2018", "31.12.2017"));
+        assertEquals(0,  DatFunk.JahreDifferenz("31.12.2017", "01.01.2018"));
+  
+    }
+
+}
