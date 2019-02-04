b09016afbbc88fe05ffe2a0d83cbdafecbfba7b7
Astrid Sawatzky <Astrid.Sawatzky@gmx.de>
2019-02-04 15:18:07 +0100

    enable logging also from within eclipse

--- TheraPi/src/theraPi/TheraPi.java
+++ TheraPi/src/theraPi/TheraPi.java
@@ -5,24 +5,23 @@ import java.io.IOException;
 
 import org.ini4j.Ini;
 import org.ini4j.InvalidFileFormatException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import environment.Path;
 import hauptFenster.Reha;
-import logging.Logging;
 import mandant.Mandant;
+import uk.org.lidalia.sysoutslf4j.context.SysOutOverSLF4J;
 
 public class TheraPi {
 
     public static String proghome;
 
-    private static Logger logger = LoggerFactory.getLogger(Reha.class);
+   
 
     public static void main(String[] args) throws InvalidFileFormatException, IOException {
-        new Logging("reha");
+        SysOutOverSLF4J.sendSystemOutAndErrToSLF4J();
+      
         setLookAndFeel();
-
+       
         proghome = Path.Instance.getProghome();
 
         String iniUrl = proghome + "ini/mandanten.ini";
@@ -37,10 +36,10 @@ public class TheraPi {
             mandantSelector.setLocationRelativeTo(null);
             mandantSelector.setVisible(true);
             Mandant current = mandantSelector.chosen();
-            logger.debug(current.toString());
+       //     logger.debug(current.toString());
             startReha(current);
         } else {
-            logger.debug(liste.defaultMandant().toString());
+         //   logger.debug(liste.defaultMandant().toString());
             startReha(liste.defaultMandant());
 
         }
@@ -61,7 +60,7 @@ public class TheraPi {
                 } catch (Exception justLog) {
                     // cannot happen, we searched, if it is there, but anyways
                     // ignore if it cannot be set default will be used, so we ignore
-                    logger.debug("plasticxp L&F could not be set", justLog);
+           //         logger.debug("plasticxp L&F could not be set", justLog);
                 }
 
                 break;
