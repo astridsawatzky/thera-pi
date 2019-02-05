0b97aa6fd30470a023f1ead3aedf211122f92220
Astrid Sawatzky <Astrid.Sawatzky@gmx.de>
2019-02-05 21:29:02 +0100

    code beautify

--- TheraPi/src/theraPi/MandantList.java
+++ TheraPi/src/theraPi/MandantList.java
@@ -9,25 +9,30 @@ import org.ini4j.Ini;
 import mandant.Mandant;
 
 class MandantList {
-	List<Mandant> list;
+    private static final String THERA_PI_MANDANTEN = "TheraPiMandanten";
+    private static final String ANZAHL_MANDANTEN = "AnzahlMandanten";
+    private static final String DEFAULT_MANDANT = "DefaultMandant";
+    private static final String AUSWAHL_IMMER_ZEIGEN = "AuswahlImmerZeigen";
+    List<Mandant> list;
 	private int defIndex=0;
 	private boolean showAlways;
 	public MandantList(Ini ini) {
-		int anzahlMandanten = Integer.parseInt(ini.get("TheraPiMandanten", "AnzahlMandanten"));
-		defIndex =  Integer.parseInt(ini.get("TheraPiMandanten", "DefaultMandant"));
+		int anzahlMandanten = Integer.parseInt(ini.get(THERA_PI_MANDANTEN, ANZAHL_MANDANTEN));
+		defIndex =  Integer.parseInt(ini.get(THERA_PI_MANDANTEN, DEFAULT_MANDANT));
 		list = new LinkedList<Mandant>();
 		for(int i = 1; i <= anzahlMandanten;i++){
-			String ik = new String(ini.get("TheraPiMandanten", "MAND-IK"+(i)));
-			String name = new String(ini.get("TheraPiMandanten", "MAND-NAME"+(i)));
+			String ik = ini.get(THERA_PI_MANDANTEN, "MAND-IK" + (i));
+			String name = ini.get(THERA_PI_MANDANTEN, "MAND-NAME" + (i));
 			list.add(i-1, new Mandant(ik,name));
 
 		}
 		setShowAllways(ini);
 
+
 	}
 
 	private void setShowAllways(Ini ini) {
-		String auswahlZeigen = ini.get("TheraPiMandanten", "AuswahlImmerZeigen");
+		String auswahlZeigen = ini.get(THERA_PI_MANDANTEN, AUSWAHL_IMMER_ZEIGEN);
 		if (auswahlZeigen==null) {
 			showAlways = true;
 		} else if("1".equals(auswahlZeigen)){
