dace26a9c6220b270773f6e8672171bd94ad0ecd
Astrid Sawatzky <astrid.sawatzky@gmx.de>
2019-04-13 17:44:57 +0200

    brooming

--- Reha/src/hauptFenster/login/LoginSmartDialog.java
+++ Reha/src/hauptFenster/login/LoginSmartDialog.java
@@ -14,28 +14,20 @@ public class LoginSmartDialog extends RehaSmartDialog {
     public LoginSmartDialog(JXFrame owner, String name, boolean fullsize) {
         super(owner, name);
         setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
-        RehaTP jtp = new LoginFrame( "PasswortDialog"+WinNum.NeueNummer());
-        
-        
+        RehaTP jtp = new LoginFrame("PasswortDialog" + WinNum.NeueNummer());
+
         jtp.setVisible(true);
         setAlwaysOnTop(false);
-        if(fullsize){
-                Dimension ssize = Toolkit.getDefaultToolkit().getScreenSize();
-                setSize(new Dimension(ssize.width,ssize.height));
-        }else{
-                setSize(new Dimension(700,300));
+        if (fullsize) {
+            setSize(Toolkit.getDefaultToolkit().getScreenSize());
+        } else {
+            setSize(new Dimension(700, 300));
         }
         setModal(true);
 
         setContentPanel(jtp.getContentContainer());
-        Toolkit toolkit = Toolkit.getDefaultToolkit();
-        Dimension screenSize = toolkit.getScreenSize();
-        //Calculate the frame location
-        int x = (screenSize.width - getWidth()) / 2;
-        int y = (screenSize.height - getHeight()) / 2;
-        setLocation(x, y);
-       
-       
+        setLocationRelativeTo(null);
+
     }
 
 }
