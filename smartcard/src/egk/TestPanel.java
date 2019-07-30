193bf0c9ae078bee6b247944ab555654c96bd6c4
Astrid Sawatzky <astrid.sawatzky@gmx.de>
2019-07-30 10:01:02 +0200

    egkTestpanel on click on Label copy terminal name to clipboard

--- smartcard/src/egk/TestPanel.java
+++ smartcard/src/egk/TestPanel.java
@@ -2,6 +2,11 @@ package egk;
 
 import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.Toolkit;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.List;
 
 import javax.smartcardio.CardException;
@@ -53,11 +58,29 @@ public class TestPanel extends JPanel {
 
         for (CardTerminal terminal : list) {
 
-            JLabel label = new JLabel(terminal.getName());
+            JLabel label = new JLabel(terminal.getName()) {
+                @Override
+                public String toString() {
+                    return getText();
+                }
+            };
             label.setBounds(10, y += 60, 290, 64);
-
+            MouseListener copytoclipboard = new MouseAdapter() {
+                @Override
+                public void mouseClicked(MouseEvent e) {
+                    Toolkit.getDefaultToolkit()
+                    .getSystemClipboard()
+                    .setContents(
+                            new StringSelection(e.getSource().toString()),
+                            null
+                    );
+                    super.mouseClicked(e);
+                }
+            };
+            label.addMouseListener(copytoclipboard );
             TestLabel lblNewLabel = new TestLabel("nix los hier");
             lblNewLabel.setBounds(300, y, 131, 64);
+
             add(label);
             add(lblNewLabel);
             EgkReader egk = new EgkReader(terminal.getName());
