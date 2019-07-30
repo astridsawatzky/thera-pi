c97ca7cfdf5fbf3d47ca9b943686881e596279c7
Astrid Sawatzky <astrid.sawatzky@gmx.de>
2019-07-30 21:24:56 +0200

    Versionnummer auf 1.1.1

--- Reha/src/hauptFenster/Version.java
+++ Reha/src/hauptFenster/Version.java
@@ -2,13 +2,13 @@ package hauptFenster;
 
 public class Version {
 
-    public static final String aktuelleVersion = "2019-06-06-DB=";
+    public static final String aktuelleVersion = "2019-07-30-DB=";
     public static final int major = 1;
     public static final int minor = 1;
-    public static final int revision = 0;
+    public static final int revision = 1;
 
     public static String number() {
-        return String.format("%d.%d", major, minor);
+        return String.format("%d.%d.%d", major, minor,revision);
     }
 
 }
