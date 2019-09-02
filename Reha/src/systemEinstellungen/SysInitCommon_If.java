9fbefd7577bd300465bd9cd703eb4d96b1ae856a
McM <McM@shared-files.de>
2019-09-02 18:25:27 +0200

    class abbruch... aus fb_verkaufsmodul kopiert

---
+++ Reha/src/systemEinstellungen/SysInitCommon_If.java
@@ -0,0 +1,9 @@
+package systemEinstellungen;
+
+public interface SysInitCommon_If {
+
+	public abstract void Abbruch();
+	public abstract void Speichern();
+	public abstract void AddEntry(int instanceNb);
+	public abstract void RemoveEntry(int instanceNb);
+}
