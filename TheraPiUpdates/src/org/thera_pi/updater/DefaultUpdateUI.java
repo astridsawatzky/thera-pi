bb0442aea67eb9858318e5580f22a0c10e874c22
Astrid Sawatzky <astrid.sawatzky@gmx.de>
2019-10-29 17:21:45 +0100

    Defaultimpl UpdateUI initial commit

---
+++ TheraPiUpdates/src/org/thera_pi/updater/DefaultUpdateUI.java
@@ -0,0 +1,51 @@
+package org.thera_pi.updater;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import javax.swing.SwingUtilities;
+
+import com.sun.javafx.tk.Toolkit;
+
+import javafx.application.Platform;
+import javafx.embed.swing.JFXPanel;
+import javafx.scene.control.ChoiceDialog;
+
+public class DefaultUpdateUI extends ChoiceDialog<UpdateConsent> implements UpdateUI {
+
+    private static UpdateConsent both = new UpdateConsent(true, true);
+    private static UpdateConsent downloadOnly = new UpdateConsent(true, false);
+    private static UpdateConsent donothing = new UpdateConsent(false, false);
+
+    public DefaultUpdateUI() {
+        super(both, both, donothing, downloadOnly);
+    }
+
+    @Override
+    public UpdateConsent askForConsent() {
+
+
+        return showAndWait().get();
+    }
+
+public static void main(String[] args) throws InterruptedException {
+    initToolkit();
+    Platform.runLater(() -> {
+    DefaultUpdateUI dialog = new DefaultUpdateUI();
+    dialog.askForConsent();
+    });
+}
+private static void initToolkit() throws InterruptedException
+{
+    final CountDownLatch latch = new CountDownLatch(1);
+    SwingUtilities.invokeLater(() -> {
+        new JFXPanel(); // initializes JavaFX environment
+        latch.countDown();
+    });
+
+    if (!latch.await(5L, TimeUnit.SECONDS))
+        throw new ExceptionInInitializerError();
+}
+}
+
+
--- TheraPiUpdates/src/org/thera_pi/updater/UpdateConsent.java
+++ TheraPiUpdates/src/org/thera_pi/updater/UpdateConsent.java
@@ -1,5 +1,7 @@
 package org.thera_pi.updater;
 
+import java.util.Objects;
+
 public class UpdateConsent {
     private final boolean download;
     private final boolean update;
@@ -16,5 +18,31 @@ public class UpdateConsent {
      boolean allowsInstall() {
         return update;
     }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(download, update);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        UpdateConsent other = (UpdateConsent) obj;
+        return download == other.download && update == other.update;
+    }
+
+    @Override
+    public String toString() {
+        String result = download ? "herunterladen" : "";
+        result += update? " und aktualisieren":"";
+
+        return result.isEmpty()? "nix machen" : result;
+    }
+
 
 }
