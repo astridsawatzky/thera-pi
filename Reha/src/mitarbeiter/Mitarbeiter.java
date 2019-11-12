6eda4dd6ca1ddd376b08777ade546e7be8f35f4f
Astrid Sawatzky <astrid.sawatzky@gmx.de>
2019-11-12 10:23:33 +0100

    setters und getters und savedto

--- Reha/src/mitarbeiter/Mitarbeiter.java
+++ Reha/src/mitarbeiter/Mitarbeiter.java
@@ -22,7 +22,145 @@ public class Mitarbeiter {
     String    abteilung;
     int    deftakt;
     int    kalzeile;
-    int    id;
+    int    id =0;
+    boolean isdirty=true;
+    public String getAnrede() {
+        return anrede;
+    }
+    public void setAnrede(String anrede) {
+        this.anrede = anrede;
+        isdirty=true;
+    }
+    public String getVorname() {
+        return vorname;
+    }
+    public void setVorname(String vorname) {
+        this.vorname = vorname;
+        isdirty=true;
+    }
+    public String getNachname() {
+        return nachname;
+    }
+    public void setNachname(String nachname) {
+        this.nachname = nachname;
+        isdirty=true;
+    }
+    public String getStrasse() {
+        return strasse;
+    }
+    public void setStrasse(String strasse) {
+        this.strasse = strasse;
+        isdirty=true;
+    }
+    public String getPlz() {
+        return plz;
+    }
+    public void setPlz(String plz) {
+        this.plz = plz;
+        isdirty=true;
+    }
+    public String getOrt() {
+        return ort;
+    }
+    public void setOrt(String ort) {
+        this.ort = ort;
+        isdirty=true;
+    }
+    public String getTelefon1() {
+        return telefon1;
+    }
+    public void setTelefon1(String telefon1) {
+        this.telefon1 = telefon1;
+        isdirty=true;
+    }
+    public String getTelfon2() {
+        return telfon2;
+    }
+    public void setTelfon2(String telfon2) {
+        this.telfon2 = telfon2;
+        isdirty=true;
+    }
+    public LocalDate getGeboren() {
+        return geboren;
+    }
+    public void setGeboren(LocalDate geboren) {
+        this.geboren = geboren;
+        isdirty=true;
+    }
+    public String getMatchcode() {
+        return matchcode;
+    }
+    public void setMatchcode(String matchcode) {
+        this.matchcode = matchcode;
+        isdirty=true;
+    }
+    public String getZtext() {
+        return ztext;
+    }
+    public void setZtext(String ztext) {
+        this.ztext = ztext;
+        isdirty=true;
+    }
+    public int getKal_teil() {
+        return kal_teil;
+    }
+    public void setKal_teil(int kal_teil) {
+        this.kal_teil = kal_teil;
+        isdirty=true;
+    }
+    public int getPers_nr() {
+        return pers_nr;
+    }
+    public void setPers_nr(int pers_nr) {
+        this.pers_nr = pers_nr;
+        isdirty=true;
+    }
+    public double getAstunden() {
+        return astunden;
+    }
+    public void setAstunden(double astunden) {
+        this.astunden = astunden;
+        isdirty=true;
+    }
+    public boolean isNicht_zeig() {
+        return nicht_zeig;
+    }
+    public void setNicht_zeig(boolean nicht_zeig) {
+        this.nicht_zeig = nicht_zeig;
+        isdirty=true;
+    }
+    public String getAbteilung() {
+        return abteilung;
+    }
+    public void setAbteilung(String abteilung) {
+        this.abteilung = abteilung;
+        isdirty=true;
+    }
+    public int getDeftakt() {
+        return deftakt;
+    }
+    public void setDeftakt(int deftakt) {
+        this.deftakt = deftakt;
+        isdirty=true;
+    }
+    public int getKalzeile() {
+        return kalzeile;
+    }
+    public void setKalzeile(int kalzeile) {
+        this.kalzeile = kalzeile;
+        isdirty=true;
+    }
+    public boolean isIsdirty() {
+        return isdirty;
+    }
+
+    void clean() {
+        if(!isNew())
+        isdirty=false;
+    }
+    public boolean isNew() {
+        return id == 0;
+    }
 
 
 
--- Reha/src/mitarbeiter/MitarbeiterDto.java
+++ Reha/src/mitarbeiter/MitarbeiterDto.java
@@ -6,10 +6,14 @@ import java.sql.SQLException;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.sql.Statement;
 
 import mandant.IK;
 import sql.DatenquellenFactory;
@@ -18,7 +22,7 @@ public class MitarbeiterDto {
 
     private final IK ik;
     private Logger logger = LoggerFactory.getLogger(MitarbeiterDto.class);
-    private String sql = "SELECT * FROM kollegen2";
+    private String SelectAllSql = "SELECT * FROM kollegen2";
 
     public MitarbeiterDto(IK ik) {
         this.ik = ik;
@@ -30,12 +34,13 @@ public class MitarbeiterDto {
         try (Connection con = new DatenquellenFactory().with(ik)
                                                        .createConnection();) {
             ResultSet rs = con.createStatement()
-                              .executeQuery(sql);
+                              .executeQuery(SelectAllSql);
             while (rs.next()) {
 
                 mitarbeiterListe.add(ofResultset(rs));
 
             }
+
             return mitarbeiterListe;
         } catch (SQLException e) {
             logger.error("could not retrieve Mitarbeiter from Database", e);
@@ -43,7 +48,24 @@ public class MitarbeiterDto {
         }
 
     }
+    public Optional<Mitarbeiter> byMatchcode(String matchcode) {
+        String sql = "SELECT * FROM kollegen2 WHERE matchcode LIKE '" + matchcode + "';";
+        Mitarbeiter mitarbeiter = null;
+        try (Connection con = new DatenquellenFactory().with(ik)
+                                                       .createConnection();
 
+                ResultSet rs = con.createStatement()
+                                  .executeQuery(sql);) {
+            if (rs.next()) {
+                mitarbeiter = ofResultset(rs);
+
+            }
+        } catch (SQLException e) {
+            logger.error("could not retrieve Mitarbeiter from Database", e);
+        }
+
+        return Optional.ofNullable(mitarbeiter);
+    }
     private Mitarbeiter ofResultset(ResultSet rs) throws SQLException {
         Mitarbeiter ma = new Mitarbeiter();
         ma.anrede = rs.getString("ANREDE");
@@ -54,8 +76,9 @@ public class MitarbeiterDto {
         ma.ort = rs.getString("ORT");
         ma.telefon1 = rs.getString("TELEFON1");
         ma.telfon2 = rs.getString("TELFON2");
-        ma.geboren = rs.getDate("GEBOREN")==null?null:rs.getDate("GEBOREN")
-                       .toLocalDate();
+        ma.geboren = rs.getDate("GEBOREN") == null ? null
+                : rs.getDate("GEBOREN")
+                    .toLocalDate();
         ma.matchcode = rs.getString("matchcode");
         ma.ztext = rs.getString("ZTEXT");
         ma.kal_teil = rs.getInt("KAL_TEIL");
@@ -68,12 +91,162 @@ public class MitarbeiterDto {
         ma.deftakt = rs.getInt("DEFTAKT");
         ma.kalzeile = rs.getInt("KALZEILE");
         ma.id = rs.getInt("ID");
-
+        ma.isdirty = false;
         return ma;
+    }
+
+    void save(List<Mitarbeiter> mitarbeiterListe) {
+        mitarbeiterListe.get(1)
+                        .setNicht_zeig(!mitarbeiterListe.get(1).nicht_zeig);
+        Map<Boolean, List<Mitarbeiter>> mitarbeiterparts = mitarbeiterListe.stream()
+                                                                           .filter(m -> m.isdirty)
+                                                                           .collect(Collectors.partitioningBy(
+                                                                                   Mitarbeiter::isNew));
+
+        List<String> mitarbeiterUpdateSql = mitarbeiterparts.get(false)
+                                                            .stream()
+                                                            .map(m -> generateUpdateSQL(m))
+                                                            .collect(Collectors.toList());
+        logger.debug("updating " + mitarbeiterUpdateSql.size() + " Mitarbeiter in Database");
+
+        try (Connection con = new DatenquellenFactory().with(ik)
+                                                       .createConnection();
+                Statement stmt = con.createStatement()) {
+
+            for (String string : mitarbeiterUpdateSql) {
+                stmt.addBatch(string);
+            }
+            stmt.executeBatch();
+        } catch (SQLException e) {
+            logger.error("Fehler beim Speichern der Mitarbeiter", e);
+        }
+
+        List<Mitarbeiter> list = mitarbeiterparts.get(true);
+        logger.debug("inserting " + list.size() + " Mitarbeiter into Database");
+
+        try (Connection con = new DatenquellenFactory().with(ik)
+                                                       .createConnection();
+                Statement statement = con.createStatement();) {
+
+            for (Mitarbeiter mitarbeiter : list) {
+
+                statement.executeUpdate(generateInsertSQL(mitarbeiter), Statement.RETURN_GENERATED_KEYS);
+
+                ResultSet rs = statement.getGeneratedKeys();
+                rs.next();
+                int id = rs.getInt(1);
+                mitarbeiter.id = id;
+            }
+        } catch (SQLException e) {
+            logger.error("fehler beim einfügen neuer mitarbeiter", e);
+        }
+
+    }
+
+    private String generateInsertSQL(Mitarbeiter ma) {
+        String sqlStart = "INSERT INTO Kollegen2 ( ANREDE, VORNAME , NACHNAME, STRASSE , PLZ , ORT , TELEFON1 , TELFON2, GEBOREN , matchcode , ZTEXT , KAL_TEIL , PERS_NR , ASTUNDEN, NICHT_ZEIG , ABTEILUNG , DEFTAKT , KALZEILE ) VALUES (";
+        String sql = new StringBuilder().append(sqlStart)
+                                        .append(einklammern(ma.anrede))
+                                        .append(",")
+                                        .append(einklammern(ma.vorname))
+                                        .append(",")
+                                        .append(einklammern(ma.nachname))
+                                        .append(",")
+                                        .append(einklammern(ma.strasse))
+                                        .append(",")
+                                        .append(einklammern(ma.plz))
+                                        .append(",")
+                                        .append(einklammern(ma.ort))
+                                        .append(",")
+                                        .append(einklammern(ma.telefon1))
+                                        .append(",")
+                                        .append(einklammern(ma.telfon2))
+                                        .append(",")
+                                        .append(ma.geboren)
+                                        .append(",")
+                                        .append(einklammern(ma.matchcode))
+                                        .append(",")
+                                        .append(einklammern(ma.ztext))
+                                        .append(",")
+                                        .append(einklammern(ma.kal_teil))
+                                        .append(",")
+                                        .append(einklammern(ma.pers_nr))
+                                        .append(",")
+                                        .append(ma.astunden)
+                                        .append(",")
+                                        .append((ma.nicht_zeig ? "'T'" : "'F'"))
+                                        .append(",")
+                                        .append(einklammern(ma.abteilung))
+                                        .append(",")
+                                        .append(ma.deftakt)
+                                        .append(",")
+                                        .append(ma.kalzeile)
+                                        .append(")")
+                                        .toString();
+
+        return sql;
+    }
+
+    private String generateUpdateSQL(Mitarbeiter ma) {
+        String sql = new StringBuilder().append("UPDATE Kollegen2")
+                                        .append(" SET ANREDE = ")
+                                        .append(einklammern(ma.anrede))
+                                        .append(", VORNAME = ")
+                                        .append(einklammern(ma.vorname))
+                                        .append(", NACHNAME = ")
+                                        .append(einklammern(ma.nachname))
+                                        .append(", STRASSE = ")
+                                        .append(einklammern(ma.strasse))
+                                        .append(", PLZ = ")
+                                        .append(einklammern(ma.plz))
+                                        .append(", ORT = ")
+                                        .append(einklammern(ma.ort))
+                                        .append(", TELEFON1 = ")
+                                        .append(einklammern(ma.telefon1))
+                                        .append(", TELFON2 = ")
+                                        .append(einklammern(ma.telfon2))
+                                        .append(", GEBOREN = ")
+                                        .append(ma.geboren)
+                                        .append(", matchcode = ")
+                                        .append(einklammern(ma.matchcode))
+                                        .append(", ZTEXT = ")
+                                        .append(einklammern(ma.ztext))
+                                        .append(", KAL_TEIL = ")
+                                        .append(einklammern(ma.kal_teil))
+                                        .append(", PERS_NR = ")
+                                        .append(einklammern(ma.pers_nr))
+                                        .append(", ASTUNDEN = ")
+                                        .append(ma.astunden)
+                                        .append(", NICHT_ZEIG = ")
+                                        .append((ma.nicht_zeig ? "'T'" : "'F'"))
+                                        .append(", ABTEILUNG = ")
+                                        .append(einklammern(ma.abteilung))
+                                        .append(", DEFTAKT = ")
+                                        .append(ma.deftakt)
+                                        .append(", KALZEILE = ")
+                                        .append(ma.kalzeile)
+                                        .append(" WHERE ID = ")
+                                        .append(ma.id)
+                                        .toString();
+        return sql;
+    }
+
+    private Integer einklammern(int invalue) {
+
+        return invalue == 0 ? null : Integer.valueOf(invalue);
+    }
+
+    private String einklammern(String value) {
+        return value == null ? null : "'" + value + "'";
     }
 
     public static void main(String[] args) {
         List<Mitarbeiter> result = new MitarbeiterDto(new IK("987654321")).all();
-
+        Mitarbeiter e = new Mitarbeiter();
+        e.nachname = "insertme";
+        result.add(e);
+        result.add(e);
+        new MitarbeiterDto(new IK("987654321")).save(result);
     }
+
 }
