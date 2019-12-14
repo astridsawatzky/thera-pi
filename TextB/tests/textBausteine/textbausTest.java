cdb27560eed7b67511e288d934fe1bcc86ad89c2
Astrid Sawatzky <astrid.sawatzky@gmx.de>
2019-12-14 10:35:45 +0100

    convenience main to start textbausteine

---
+++ TextB/tests/textBausteine/textbausTest.java
@@ -0,0 +1,8 @@
+package textBausteine;
+
+
+public class textbausTest {
+    public static void main(String[] args) {
+        textbaus.main(new String[] { "C:/RehaVerwaltung/ini/987654321/rehajava.ini", "C:/RehaVerwaltung/ini/987654321/thbericht.ini", "6000" });
+    }
+}
