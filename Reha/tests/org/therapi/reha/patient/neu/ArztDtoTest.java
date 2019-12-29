98d6dbb4da71f4c37dfff748a0cb30ce9ebf7e83
Astrid Sawatzky <astrid.sawatzky@gmx.de>
2019-12-29 20:58:40 +0100

    roundtriptest alle Aerzte in DB

--- Reha/tests/org/therapi/reha/patient/neu/ArztDtoTest.java
+++ Reha/tests/org/therapi/reha/patient/neu/ArztDtoTest.java
@@ -1,11 +1,16 @@
 package org.therapi.reha.patient.neu;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.Optional;
 
 import org.junit.Test;
 
+import sql.DatenquellenFactory;
 
 public class ArztDtoTest {
 
@@ -17,11 +22,39 @@ public class ArztDtoTest {
         assertEquals(in.toString(), out.toString());
 
     }
+
     @Test
     public final void testFindbyId() throws Exception {
 
-      Optional<ArztDto> result = ArztDto.findbyID("1", "987654321");
-       assertTrue(result.isPresent());
+        Optional<ArztDto> result = ArztDto.findbyID("1", "987654321");
+        assertTrue(result.isPresent());
     }
 
+    @Test
+    public void roundtripDtoToArztVonDB() throws Exception {
+
+        ArztDto in = ArztDto.findbyID("1", "987654321")
+                            .get();
+        ArztDto out = new ArztDto(in.toArzt());
+        assertEquals(in.toString(), out.toString());
+
+    }
+
+    @Test
+    public void loadeachArzt() throws Exception {
+        String sql = "select  id from arzt;";
+        String ik = "987654321";
+        DatenquellenFactory dqf = new DatenquellenFactory(ik);
+        Connection con = dqf.createConnection();
+        Statement stmt = con.createStatement();
+        ResultSet rs = stmt.executeQuery(sql);
+        while (rs.next()) {
+
+            ArztDto in = ArztDto.findbyID(rs.getString("iD"), ik)
+                                .get();
+            ArztDto out = new ArztDto(in.toArzt());
+            assertEquals(in.toString(), out.toString());
+
+        }
+    }
 }
