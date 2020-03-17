99944280df65b99972b6097a85bbd193d5a4b3f8
Astrid Sawatzky <astrid.sawatzky@gmx.de>
2020-03-17 18:20:34 +0100

    brooming

---
+++ Reha/src/systemEinstellungen/config/DBKontakt.java
@@ -0,0 +1,9 @@
+package systemEinstellungen.config;
+
+class DBKontakt {
+    protected String decryptedPassword;
+    protected String user;
+    protected String typ;
+    protected String jdbcDB;
+    protected String treiber;
+}
--- Reha/src/systemEinstellungen/config/Datenbank.java
+++ Reha/src/systemEinstellungen/config/Datenbank.java
@@ -1,8 +1,5 @@
 package systemEinstellungen.config;
 
-import java.util.ArrayList;
-import java.util.Vector;
-
 import javax.swing.JOptionPane;
 
 import CommonTools.INIFile;
@@ -11,72 +8,61 @@ import crypt.Verschluesseln;
 
 public class Datenbank {
 
-    private static Vector<ArrayList<String>> vDatenBank = new Vector<ArrayList<String>>();
-
-//    public static Vector<ArrayList<String>> getvDatenBank() {
-//        return vDatenBank;
-//    }
+    private static DBKontakt contact = new DBKontakt();
 
     public String treiber() {
-        return vDatenBank.get(0)
-                         .get(0);
+        return contact.treiber;
     }
 
     public String password() {
-        return Datenbank.vDatenBank.get(0)
-                                   .get(4);
+        return contact.decryptedPassword;
     }
 
     public String user() {
-        return Datenbank.vDatenBank.get(0)
-                                   .get(3);
+        return contact.user;
     }
 
     public String jdbcDB() {
-        return Datenbank.vDatenBank.get(0)
-                                   .get(1);
+        return contact.jdbcDB;
+    }
+
+    public String typ() {
+        return contact.typ;
     }
 
     public void datenbankEinstellungeneinlesen(INIFile inifile) {
 
-        int lesen = inifile.getIntegerProperty("DatenBank", "AnzahlConnections");
-        for (int i = 1; i < (lesen + 1); i++) {
-            ArrayList<String> aKontakt = new ArrayList<String>();
-            aKontakt.add(String.valueOf(inifile.getStringProperty("DatenBank", "DBTreiber" + i)));
-            aKontakt.add(String.valueOf(inifile.getStringProperty("DatenBank", "DBKontakt" + i)));
-            aKontakt.add(String.valueOf(inifile.getStringProperty("DatenBank", "DBType" + i)));
-            String sbenutzer = String.valueOf(inifile.getStringProperty("DatenBank", "DBBenutzer" + i));
-            aKontakt.add(String.valueOf(sbenutzer));
-            String pw = String.valueOf(inifile.getStringProperty("DatenBank", "DBPasswort" + i));
-            String decrypted = null;
-            if (pw == null) {
-                decrypted = String.valueOf("");
-                JOptionPane.showMessageDialog(null, "Passwort der MySql-Datenbank = null");
-            } else if (!pw.equals("")) {
-                Verschluesseln man = Verschluesseln.getInstance();
-                decrypted = man.decrypt(pw);
-            } else {
-                Object ret = JOptionPane.showInputDialog(null,
-                        "Geben Sie bitte das Passwort für die MySql-Datenbank ein", "");
-                if (ret == null) {
-                    decrypted = String.valueOf("");
-                } else {
-                    decrypted = ((String) ret).trim();
-                    Verschluesseln man = Verschluesseln.getInstance();
-                    inifile.setStringProperty("DatenBank", "DBPasswort" + i, man.encrypt(((String) ret)), null);
-                    INITool.saveIni(inifile);
-                }
-            }
+        contact.treiber = inifile.getStringProperty("DatenBank", "DBTreiber1");
+        contact.jdbcDB = inifile.getStringProperty("DatenBank", "DBKontakt1");
+        contact.typ = inifile.getStringProperty("DatenBank", "DBType1");
+        contact.user = inifile.getStringProperty("DatenBank", "DBBenutzer1");
 
-            aKontakt.add(decrypted);
-            vDatenBank.add(aKontakt);
+        String pw = inifile.getStringProperty("DatenBank", "DBPasswort1");
+        contact.decryptedPassword = processPassword(inifile, pw);
 
-        }
     }
 
-    public String typ() {
-        return vDatenBank.get(0)
-                         .get(2);
+    protected String processPassword(INIFile inifile, String pw) {
+        String decryptedPassword;
+        if (pw == null) {
+            decryptedPassword = "";
+            JOptionPane.showMessageDialog(null, "Passwort der MySql-Datenbank = null");
+        } else if ("".equals(pw)) {
+            String ret = JOptionPane.showInputDialog(null, "Geben Sie bitte das Passwort für die MySql-Datenbank ein",
+                    "");
+            if (ret == null) {
+                decryptedPassword = "";
+            } else {
+                decryptedPassword = ret.trim();
+                Verschluesseln man = Verschluesseln.getInstance();
+                inifile.setStringProperty("DatenBank", "DBPasswort1", man.encrypt(ret), null);
+                INITool.saveIni(inifile);
+            }
+        } else {
+            Verschluesseln man = Verschluesseln.getInstance();
+            decryptedPassword = man.decrypt(pw);
+        }
+        return decryptedPassword;
     }
 
 }
