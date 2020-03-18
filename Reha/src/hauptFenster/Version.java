ce36f9bb44dc12a711d44d00719bf95a5f0d5092
Astrid Sawatzky <astrid.sawatzky@gmx.de>
2020-03-18 17:50:31 +0100

    Release 1.1.4

--- Reha/src/hauptFenster/Version.java
+++ Reha/src/hauptFenster/Version.java
@@ -2,10 +2,10 @@ package hauptFenster;
 
 public class Version {
 
-    public static final String aktuelleVersion = "2019-12-14-DB=";
+    public static final String aktuelleVersion = "2020-03-18-DB=";
     public static final int major = 1;
     public static final int minor = 1;
-    public static final int revision = 3;
+    public static final int revision = 4;
 
     public static String number() {
         return String.format("%d.%d.%d", major, minor, revision);
