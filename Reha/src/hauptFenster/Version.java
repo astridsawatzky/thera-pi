a0e36218460a9cc142437fa9fc35bdd3921e90ea
Astrid Sawatzky <astrid.sawatzky@gmx.de>
2020-04-03 10:16:17 +0200

    Version 1.1.5

--- Reha/src/hauptFenster/Version.java
+++ Reha/src/hauptFenster/Version.java
@@ -5,7 +5,7 @@ public class Version {
     public static final String aktuelleVersion = "2020-03-18-DB=";
     public static final int major = 1;
     public static final int minor = 1;
-    public static final int revision = 4;
+    public static final int revision = 5;
 
     public static String number() {
         return String.format("%d.%d.%d", major, minor, revision);
