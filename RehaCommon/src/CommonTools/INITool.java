901ea115b483b2e6e19257dd93edf17fe4526ea7
Astrid Sawatzky <astrid.sawatzky@gmx.de>
2020-04-09 12:39:03 +0200

    use set instead of array, simplify logic

--- RehaCommon/src/CommonTools/INITool.java
+++ RehaCommon/src/CommonTools/INITool.java
@@ -2,52 +2,36 @@ package CommonTools;
 
 import java.io.File;
 import java.io.InputStream;
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 import org.ini4j.Ini;
 
 public class INITool {
-  private  static String[] dbInis = new String[] { "nix" };
     static Set<String> inisInDb = new HashSet<>();
 
-    public static void setDBInis(String[] xdbini) {
-        dbInis = xdbini;
+    public static int anzahlInisInDB() {
+        return inisInDb.size();
     }
-
-    public static int anzahlInisInDB(){
-        return dbInis.length;
-    }
-
 
     public static void init(String pfad) {
-
         INIFile file = new INIFile(pfad + "inicontrol.ini");
 
         try {
             Ini inicontrol = new Ini(new File(pfad + "inicontrol.ini"));
             int anzahl = file.getIntegerProperty("INIinDB", "INIAnzahl");
-            if (anzahl == 0) {
-                dbInis = new String[] { "nix" };
-            } else {
 
-                dbInis = new String[anzahl];
-                inisInDb.addAll(inicontrol.get("INIinDB")
-                                          .values());
-
-                for (int i = 0; i < dbInis.length; i++) {
-                    dbInis[i] = String.valueOf(file.getStringProperty("INIinDB", "DBIni" + Integer.toString(i + 1)));
-                }
-            }
+            inisInDb.addAll(inicontrol.get("INIinDB")
+                                      .values());
+            inisInDb.remove(String.valueOf(anzahl));
         } catch (Exception ex) {
-            dbInis = new String[] { "nix" };
+            ex.printStackTrace();
         }
     }
 
     /**
-     * liefert INIFile Objekt mit aktuell verwandter ini, unabhaengig ob die eine
-     * lokale Datei oder in der DB abgelegt ist
+     * Liefert INIFile Objekt mit aktuell verwandter ini, unabhaengig ob die eine
+     * lokale Datei oder in der DB abgelegt ist.
      *
      * @param path      Pfad zu lokaler ini
      * @param iniToOpen Name der ini-Datei
@@ -56,8 +40,7 @@ public class INITool {
     public static INIFile openIni(String path, String iniToOpen) {
         INIFile inif = null;
         try {
-            if (Arrays.asList(dbInis)
-                      .contains(iniToOpen)) {
+            if (inisInDb.contains(iniToOpen)) {
                 InputStream stream = SqlInfo.liesIniAusTabelle(iniToOpen);
                 inif = new INIFile(stream, iniToOpen);
             } else {
@@ -70,8 +53,8 @@ public class INITool {
     }
 
     /**
-     * liefert INIFile Objekt mit lokaler ini-Datei, falls die ini in der DB
-     * abgelegt ist, sonst null
+     * Liefert INIFile Objekt mit lokaler ini-Datei, falls die ini in der DB
+     * abgelegt ist, sonst null.
      *
      * @param path      Pfad zu lokaler ini
      * @param iniToOpen Name der ini-Datei
@@ -80,8 +63,7 @@ public class INITool {
     public static INIFile openIniFallback(String path, String iniToOpen) {
         INIFile inif = null;
         try {
-            if (Arrays.asList(dbInis)
-                      .contains(iniToOpen)) {
+            if (inisInDb.contains(iniToOpen)) {
                 inif = new INIFile(path + iniToOpen);
             }
         } catch (Exception ex) {
@@ -90,12 +72,10 @@ public class INITool {
         return inif;
     }
 
-    /************************/
     public static boolean saveIni(INIFile iniToSave) {
         boolean ret = false;
         try {
-            if (Arrays.asList(dbInis)
-                      .contains(iniToSave.getFileName())) {
+            if (inisInDb.contains(iniToSave.getFileName())) {
                 SqlInfo.schreibeIniInTabelle(iniToSave.getFileName(), iniToSave.saveToStringBuffer()
                                                                                .toString()
                                                                                .getBytes());
@@ -111,5 +91,4 @@ public class INITool {
         }
         return ret;
     }
-
 }
