3c4e55b910c50bf152bf731c85d621faf774f8ed
Keith <juthofrohe@wemel.top>
2020-06-11 18:48:40 +0200

    Added standard hashcode, equals & toString...

--- OpRgaf/src/opRgaf/Berichte/Bericht1.java
+++ OpRgaf/src/opRgaf/Berichte/Bericht1.java
@@ -1,6 +1,7 @@
 package opRgaf.Berichte;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 public class Bericht1 {
     String patIntern;
@@ -116,6 +117,40 @@ public class Bericht1 {
     }
     public void setId(int id) {
         this.id = id;
+    }
+    
+    @Override
+    public String toString() {
+        return "Bericht1 [patIntern=" + patIntern + ", berichtId=" + berichtId + ", arztNum=" + arztNum
+                + ", erstellDat=" + erstellDat + ", versandArt=" + versandArt + ", versandDat=" + versandDat
+                + ", berTyp=" + berTyp + ", berStand=" + berStand + ", berBeso=" + berBeso + ", berProg=" + berProg
+                + ", berVors=" + berVors + ", diagnose=" + diagnose + ", krBild=" + krBild + ", verfasser=" + verfasser
+                + ", rezDatum=" + rezDatum + ", id=" + id + "]";
+    }
+    
+    // Hashcode & equals do not include "id" - hope that's ok...
+    @Override
+    public int hashCode() {
+        return Objects.hash(arztNum, berBeso, berProg, berStand, berTyp, berVors, berichtId, diagnose, erstellDat,
+                krBild, patIntern, rezDatum, verfasser, versandArt, versandDat);
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Bericht1 other = (Bericht1) obj;
+        return Objects.equals(arztNum, other.arztNum) && Objects.equals(berBeso, other.berBeso)
+                && Objects.equals(berProg, other.berProg) && Objects.equals(berStand, other.berStand)
+                && Objects.equals(berTyp, other.berTyp) && Objects.equals(berVors, other.berVors)
+                && berichtId == other.berichtId && Objects.equals(diagnose, other.diagnose)
+                && Objects.equals(erstellDat, other.erstellDat) && Objects.equals(krBild, other.krBild)
+                && Objects.equals(patIntern, other.patIntern) && Objects.equals(rezDatum, other.rezDatum)
+                && Objects.equals(verfasser, other.verfasser) && Objects.equals(versandArt, other.versandArt)
+                && Objects.equals(versandDat, other.versandDat);
     }
 
 }
