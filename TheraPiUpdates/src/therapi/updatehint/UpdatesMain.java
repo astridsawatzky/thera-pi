0263e736fe4714a1a2988c1402656fda4574e554
Astrid Sawatzky <astrid.sawatzky@gmx.de>
2020-07-04 08:01:58 +0200

    brooming

--- TheraPiUpdates/src/therapi/updatehint/UpdatesMain.java
+++ TheraPiUpdates/src/therapi/updatehint/UpdatesMain.java
@@ -11,44 +11,27 @@ import org.thera_pi.updater.VersionsSieb;
 
 import com.sun.javafx.application.PlatformImpl;
 
-import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.embed.swing.JFXPanel;
 import javafx.stage.Stage;
 
 public class UpdatesMain implements Runnable {
-
-
     @Override
     public void run() {
-
-
-            try {
-
-
-                Application app2;
-                app2 = Main.class.newInstance();
-
-                Stage anotherStage = new Stage();
-                app2.start(anotherStage);
-
-            } catch ( Exception e) {
-                e.printStackTrace();
-            }
-
+        try {
+            new Main().start(new Stage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-
     public static void main(String[] args) {
-        List<File> updatefiles = new VersionsSieb(new Version()).select( new HTTPRepository().filesList());
+        List<File> updatefiles = new VersionsSieb(new Version()).select(new HTTPRepository().filesList());
 
-
-        if(!updatefiles.isEmpty()) {
+        if (updatefiles.isEmpty()) {
+            Logger logger = LoggerFactory.getLogger(UpdatesMain.class);
+            logger.debug("No Updates found for Version: " + new Version().number());
+        } else {
             Main.updatefiles = updatefiles;
             PlatformImpl.startup(new UpdatesMain());
-        } else {
-            Logger logger =LoggerFactory.getLogger(UpdatesMain.class);
-            logger.debug("No Updates found for Version: " + new Version().number());
         }
     }
 }
