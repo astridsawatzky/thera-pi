2c9d0dc9fa2795aab1d81aa1acb2b6c7d5e5ff73
Keith <juthofrohe@wemel.top>
2020-07-09 08:58:45 +0200

    Split that Q&D test into 2 to make them conform with parameter usage

--- Reha/tests/systemEinstellungen/SysUtilKostentraegerTest.java
+++ Reha/tests/systemEinstellungen/SysUtilKostentraegerTest.java
@@ -9,7 +9,7 @@ import org.junit.Test;
 public class SysUtilKostentraegerTest {
 
     @Test
-    public void testDateiNameCheck() {
+    public void testDateiNameCheckServerNewer() {
         /*
          * Caller of dateiNameCheck(g, i) currently checks prior to calling dateiNameCheck whether the
          * filenames are equal, so (currently) there are no safeguards in method dateiNameCheck that
@@ -21,7 +21,7 @@ public class SysUtilKostentraegerTest {
         String GKVFile="AO05Q220.ke0";
         String IniFile="AO05Q120.ke0";
         assertTrue("GKV hat neueres Quartal", sukt.dateiNameCheck(GKVFile, IniFile));
-        assertFalse("Lokal hat neueres Quartal", sukt.dateiNameCheck(IniFile, GKVFile));
+        
         
         GKVFile="AO05Q121.ke0";
         IniFile="AO05Q320.ke0";
@@ -31,11 +31,31 @@ public class SysUtilKostentraegerTest {
         GKVFile="AO05Q220.ke0";
         IniFile="AO05Q120.ke1";
         assertTrue("GKV hat kleinere Version im naechsten Quartal", sukt.dateiNameCheck(GKVFile, IniFile));
-        assertFalse("Lokal hat kleinere Version im naechsten Quartal", sukt.dateiNameCheck(IniFile, GKVFile));
         
         GKVFile="AO05Q220.ke1";
         IniFile="AO05Q220.ke0";
         assertTrue("GKV hat hoehere Version im gleichem Quartal", sukt.dateiNameCheck(GKVFile, IniFile));
-        assertFalse("Lokal hat hoehere Version im gleichem Quartal", sukt.dateiNameCheck(IniFile, GKVFile));
+    }
+    
+    public void testDateiNameCheckLocalNewer() {
+        /*
+         * Caller of dateiNameCheck(g, i) currently checks prior to calling dateiNameCheck whether the
+         * filenames are equal, so (currently) there are no safeguards in method dateiNameCheck that
+         * check for differences in Kasse(ntyp) or exactly same files (dateiNameCheck only considers
+         * the dates and version parts of filename).
+         */
+        SysUtilKostentraeger sukt = new SysUtilKostentraeger("Testing");
+        
+        String GKVFile="AO05Q120.ke0";
+        String IniFile="AO05Q220.ke0";
+        assertTrue("Lokal hat neueres Quartal", sukt.dateiNameCheck(GKVFile, IniFile));
+        
+        GKVFile="AO05Q120.ke1";
+        IniFile="AO05Q220.ke0";
+        assertTrue("Lokal hat kleinere Version im naechsten Quartal", sukt.dateiNameCheck(GKVFile, IniFile));
+        
+        GKVFile="AO05Q220.ke0";
+        IniFile="AO05Q220.ke1";
+        assertTrue("Lokal hat hoehere Version im gleichem Quartal", sukt.dateiNameCheck(GKVFile, IniFile));
     }
 }
