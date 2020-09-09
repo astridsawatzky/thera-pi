05efa0c9d6b5427f1d61d063386d725480d47cf4
Astrid Sawatzky <astrid.sawatzky@gmx.de>
2020-09-09 12:33:02 +0200

    test durchschleusen einer hmv durch hmv13

--- hmr/src/hmv/Hmv.java
+++ hmr/src/hmv/Hmv.java
@@ -2,11 +2,10 @@ package hmv;
 
 import java.time.LocalDate;
 import java.time.Period;
-import java.util.List;
+import java.util.Objects;
 
 import core.Arzt;
 import core.Disziplin;
-import core.Krankenkasse;
 import core.Krankenversicherung;
 import core.Patient;
 import mandant.Mandant;
@@ -30,10 +29,12 @@ public class Hmv {
 
     Behandlung beh;
 
+
+
     public Hmv(Context context) {
         mandant = context.mandant;
         patient = context.patient;
-        arzt = patient.hauptarzt.get();
+        arzt = patient.hauptarzt.orElse(null);
         kv = patient.kv;
         angelegtvon = context.user;
     }
@@ -45,6 +46,37 @@ public class Hmv {
     }
 
 
+
+    @Override
+    public String toString() {
+        return "Hmv [angelegtvon=" + angelegtvon + ", mandant=" + mandant + ", disziplin=" + disziplin + ", patient="
+                + patient + ", arzt=" + arzt + ", kv=" + kv + ", ausstellungsdatum=" + ausstellungsdatum
+                + ", maxBisStart=" + maxBisStart + ", diag=" + diag + ", beh=" + beh + "]";
+    }
+
+
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(angelegtvon, arzt, ausstellungsdatum, beh, diag, disziplin, kv, mandant, maxBisStart,
+                patient);
+    }
+
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (!(obj instanceof Hmv))
+            return false;
+        Hmv other = (Hmv) obj;
+        return Objects.equals(angelegtvon, other.angelegtvon) && Objects.equals(arzt, other.arzt)
+                && Objects.equals(ausstellungsdatum, other.ausstellungsdatum) && Objects.equals(beh, other.beh)
+                && Objects.equals(diag, other.diag) && disziplin == other.disziplin && Objects.equals(kv, other.kv)
+                && Objects.equals(mandant, other.mandant) && Objects.equals(maxBisStart, other.maxBisStart)
+                && Objects.equals(patient, other.patient);
+    }
 
 
 
