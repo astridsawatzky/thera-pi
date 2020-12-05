0eb6d914a3a429dd2b190a6849fb92a29d67a06a
Astrid Sawatzky <astrid.sawatzky@gmx.de>
2020-12-05 10:29:07 +0100

    brooming

--- Reha/src/systemEinstellungen/SysUtilVorlage.java
+++ Reha/src/systemEinstellungen/SysUtilVorlage.java
@@ -1,10 +1,6 @@
 package systemEinstellungen;
 
 import java.awt.BorderLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
@@ -14,43 +10,15 @@ import org.jdesktop.swingx.JXPanel;
 
 import hauptFenster.Reha;
 
-public class SysUtilVorlage extends JXPanel implements KeyListener, ActionListener {
-    /**
-     * 
-     */
+public class SysUtilVorlage extends JXPanel {
     private static final long serialVersionUID = 1L;
 
     public SysUtilVorlage(ImageIcon img) {
         super(new BorderLayout());
-        this.setBorder(BorderFactory.createEmptyBorder(20, 40, 20, 20));
-        /****/
+        setBorder(BorderFactory.createEmptyBorder(20, 40, 20, 20));
         setBackgroundPainter(Reha.instance.compoundPainter.get("SystemInit"));
-        /****/
         JLabel jlbl = new JLabel("");
         jlbl.setIcon(img);
         add(jlbl, BorderLayout.CENTER);
-        // add(getVorlagenSeite());
-        return;
     }
-
-    @Override
-    public void keyPressed(KeyEvent e) {
-
-    }
-
-    @Override
-    public void keyReleased(KeyEvent e) {
-
-    }
-
-    @Override
-    public void keyTyped(KeyEvent e) {
-
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-
-    }
-
 }
